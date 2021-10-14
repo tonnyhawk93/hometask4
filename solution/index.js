85e89c67e9489321be28d05325f49c3a5478b174
tonnyhawk93 <tonnyhawk93@gmail.com>
2021-10-14 16:45:30 +0300

    remove increment operstion from solution/index

--- solution/index.js
+++ solution/index.js
@@ -1,14 +1,15 @@
 module.exports = function (Homework) {
+    const {add} = Homework;
     function promisify(fn, ...args) {
         return new Promise(resolve => {fn(...args, resolve)});
     }
-
     async function asyncRecursion(length, acc, currentIndex, arr, fn) {
         if(currentIndex === length) return acc; 
 
         let currentValue = await promisify(arr.get, currentIndex);
-        let result = await promisify(fn, acc, currentValue, currentIndex, arr);        
-        return await asyncRecursion(length, result, ++currentIndex, arr, fn)
+        let result = await promisify(fn, acc, currentValue, currentIndex, arr);  
+        let nexIndex = await promisify(add, currentIndex, 1)     
+        return await asyncRecursion(length, result, nexIndex, arr, fn)
     }
     return (array, fn, initialValue, cb) => {        
         promisify(array.length)
